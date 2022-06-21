3f38319d0144cf35f4697999851ffc868434938a
Chunhawit <pupz28@gmail.com>
2022-06-21 23:40:05 +0200

    add some HTTP methods

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 8888;
 
 app.get('/', (request, response) => {
   response.send('Hello Express');
+});
+
+app.get('/q/new', (request, response) => {
+  response.send('New Question Form');
+});
+
+app.post('/q/new', (request, response) => {
+  console.log(request.body);
+  const { title } = request.body ?? {};
+  response.send(`Submited Form ${title}`);
+});
+
+app.get('/q/:questionId', (request, response) => {
+  console.log(request.params);
+  response.send(`Question Page -> questionId: ${request.params.questionId}`);
 });
 
 app.listen(PORT, () => {
