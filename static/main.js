da28662944c2350694f3c3cc18d3998dee684445
Chunhawit <pupz28@gmail.com>
2022-06-24 11:30:35 +0200

    add floating button to scroll to the top of the page + add it in home and topic page

---
+++ static/main.js
@@ -0,0 +1,11 @@
+console.log('Hola Hola');
+
+const siteScrollTop = document.querySelector('.site-scroll-top');
+
+function scrollTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+if (!!siteScrollTop) {
+  siteScrollTop.addEventListener('click', scrollTop);
+}
