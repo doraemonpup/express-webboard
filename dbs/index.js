cca2dacdcad4d66e29128fbb030a19070f8555fd
Chunhawit <pupz28@gmail.com>
2022-06-26 23:32:29 +0200

    remove some comments

--- dbs/index.js
+++ dbs/index.js
@@ -4,7 +4,7 @@ const db = knex.default({
   client: 'mysql2',
   connection: {
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD, // test password
+    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
